e3ae9d779fa22458b57a2950f1d27416c1085aef
caoyf991025 <2634062576@qq.com>
2020-06-23 11:04:36 +0800

    logs

---
+++ pages/my/my_sunking_center/new/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="new">
+		
+		
+		<view class="user">
+			<view class="user1">
+				 <image src="../../../../static/我的页面/我的-晒单中心/圆.png" mode="" class="radius"></image>
+			   <text class="username">小懒猫</text>
+			</view>
+		</view>
+		
+		
+		
+		
+		
+		
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.user{
+		width: 90%;
+		display: flex;
+		 height: 500upx;
+		 border: #007AFF 1px solid;
+		
+		margin: 0px auto;
+	}
+	.user1{
+		margin-top: 60upx;
+	}
+	.radius{
+		height: 120upx;
+		width: 120upx;
+		justify-content: flex-start;
+	}
+</style>
